747778d488c90b3c487dbdb3df20aed5ab8fbeba
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-27 15:46:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+//search menu
+
+/*function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("mySearch");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myMenu");
+        li = ul.getElementsByTagName("li");
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }*/
+//side nav
+
+/*function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }*/
+
+//full screen nav
+
+//slide in from the side
+
+/* Open when someone clicks on the span element */
+/*function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+      }
+      
+      function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+      }*/
+
+//side down from the top 
+
+/*function openNav() {
+        document.getElementById("myNav").style.height = "100%";
+      }
+      
+     
+      function closeNav() {
+        document.getElementById("myNav").style.height = "0%";
+      }*/
+
+//open the menu with animation
+
+/*function openNav() {
+        document.getElementById("myNav").style.display = "block";
+      }
+      
+      
+      function closeNav() {
+        document.getElementById("myNav").style.display = "none";
+      }*/
+
+//off canvas menu
+
+/*function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+      }
+      
+     
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+      }*/
+//off canvas menu with opacity
+/*function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+
+function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+      }
+*/      
+      
